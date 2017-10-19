13468d7a48975cf59f90855ca22d7c3fb4427ab3
darkdrei <darkdrei88@gmail.com>
2017-10-19 11:43:30 -0500

    Almacena la informacion concerniente a los registros y nacimientos del municipio de forma especifica

--- src/logica/ManejadorMunicipio.java
+++ src/logica/ManejadorMunicipio.java
@@ -11,46 +11,49 @@ import java.util.ArrayList;
  *
  * @author dark
  */
-public  class ManejadorMunicipio {
+public class ManejadorMunicipio {
 
-        private ArrayList<Municipio> municipios;
+    private ArrayList<Municipio> municipios;
 
-        public ManejadorMunicipio() {
-            this.municipios = new ArrayList<>();
-            this.municipios.add(new logica.Municipio(0,"ACHI"));
-            this.municipios.add(new Municipio(0,"ACHÍ"));
-            this.municipios.add(new Municipio(1,"MAGANGUE"));
-            this.municipios.add(new Municipio(1,"MAGANGUÉ"));
-            this.municipios.add(new Municipio(1,"MAGUANGUE"));
-            this.municipios.add(new Municipio(2,"MONTECRISTO"));
-            this.municipios.add(new Municipio(3,"PINILLOS"));
-            this.municipios.add(new Municipio(4,"SAN JACINTO DEL CAUCA"));
-            this.municipios.add(new Municipio(5,"TIQUISIO"));
-            this.municipios.add(new Municipio(0,"ACH�"));
-            this.municipios.add(new Municipio(8,"MAR�A LA BAJA"));
-            this.municipios.add(new Municipio(8,"MARIALABAJA"));
-            this.municipios.add(new Municipio(8,"MARIA LA BAJA"));
-            this.municipios.add(new Municipio(7,"SAN JACINTO"));
-            this.municipios.add(new Municipio(6,"SAN JUAN NEPOMUCENO"));
-        }
-        
-        public String getMunicipio(int id){
-            corte:
-            for (Municipio municipio : municipios) {
-                if(municipio.getId()==id){
-                    return municipio.getNombre();
-                }            }
-            return "";
-        }
-        
-        public ArrayList<String> getMunicipios(int id){
-            ArrayList<String> mun_tem = new ArrayList<>();
-            for (Municipio mun_tem1 : this.municipios) {
-                if(mun_tem1.getId()==id && id != -1)
-                    mun_tem.add(mun_tem1.getNombre());
-                if(id==-1)
-                    mun_tem.add(mun_tem1.getNombre());
+    public ManejadorMunicipio() {
+        this.municipios = new ArrayList<>();
+        this.municipios.add(new logica.Municipio(0, "ACHI"));
+        this.municipios.add(new Municipio(0, "ACHÍ"));
+        this.municipios.add(new Municipio(1, "MAGANGUE"));
+        this.municipios.add(new Municipio(1, "MAGANGUÉ"));
+        this.municipios.add(new Municipio(1, "MAGUANGUE"));
+        this.municipios.add(new Municipio(2, "MONTECRISTO"));
+        this.municipios.add(new Municipio(3, "PINILLOS"));
+        this.municipios.add(new Municipio(4, "SAN JACINTO DEL CAUCA"));
+        this.municipios.add(new Municipio(5, "TIQUISIO"));
+        this.municipios.add(new Municipio(0, "ACH�"));
+        this.municipios.add(new Municipio(8, "MAR�A LA BAJA"));
+        this.municipios.add(new Municipio(8, "MARIALABAJA"));
+        this.municipios.add(new Municipio(8, "MARIA LA BAJA"));
+        this.municipios.add(new Municipio(7, "SAN JACINTO"));
+        this.municipios.add(new Municipio(6, "SAN JUAN NEPOMUCENO"));
+    }
+
+    public String getMunicipio(int id) {
+        corte:
+        for (Municipio municipio : municipios) {
+            if (municipio.getId() == id) {
+                return municipio.getNombre();
             }
-            return mun_tem;
         }
+        return "";
+    }
+
+    public ArrayList<String> getMunicipios(int id) {
+        ArrayList<String> mun_tem = new ArrayList<>();
+        for (Municipio mun_tem1 : this.municipios) {
+            if (mun_tem1.getId() == id && id != -1) {
+                mun_tem.add(mun_tem1.getNombre());
+            }
+            if (id == -1) {
+                mun_tem.add(mun_tem1.getNombre());
+            }
+        }
+        return mun_tem;
+    }
 }
