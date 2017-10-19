c533e7cb5aa465b3a22945eb49a08c2f94c9a18a
darkdrei <darkdrei88@gmail.com>
2017-10-19 14:02:44 -0500

    interfaz de operaciones de calidad con el fin de establecer puntos de variacion

---
+++ src/logica/OperacionesCalidad.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+/**
+ *
+ * @author E7250
+ */
+public interface OperacionesCalidad {
+    public void analizarCalidaDeLaInformacion();
+    public void analizarCalidaDeLaInformacion(Object data);
+}
