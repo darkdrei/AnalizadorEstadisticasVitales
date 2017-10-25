8a64cd18748e700572c7513f37f877be330a8032
darkdrei <darkdrei88@gmail.com>
2017-10-24 23:59:32 -0500

    se implementa la interfaz en calidad de la informacion para la clase AnalisisCalidad

--- src/logica/AnalizadorCalidad.java
+++ src/logica/AnalizadorCalidad.java
@@ -5,6 +5,8 @@
  */
 package logica;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author E7250
@@ -31,7 +33,19 @@ public class AnalizadorCalidad implements OperacionesCalidad{
 
     @Override
     public void analizarCalidaDeLaInformacion() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        for ( RegistrosMunicipio r : this.getDatos().getRegistros()){
+            int con_area_nacimiento = 0;
+            ArrayList<FilaEstadisticaVitalNacimientos> area_nacimiento = new ArrayList<>();
+            for(FilaEstadisticaVitalNacimientos n :r.getNacimientos()){
+                //System.out.println(n.getArea_nacimiento()+"  "+n.getSitio_nacimiento());
+                if (n.getArea_nacimiento().equals("RURAL DISPERSO") && n.getSitio_nacimiento().equals("INSTITUCI�N DE SALUD")){
+                    con_area_nacimiento++;
+                    area_nacimiento.add(n);
+                }
+                if(n.getSitio_nacimiento().equals("INSTITUCI�N DE SALUD"))
+                
+            }
+        }
     }
 
     @Override
