9214db56388944614991e9c2f7e96ba307fe9d2d
darkdrei <darkdrei88@gmail.com>
2017-10-29 14:23:59 -0500

    se agrega la tabla de mujeres fertelidad de defuncion

--- src/logica/CalidadWord.java
+++ src/logica/CalidadWord.java
@@ -78,7 +78,7 @@ public class CalidadWord extends Reporte {
         integrarTablaDefuncion(document, "Area de defunción", this.getArea_defuncion());
         integrarTablaDefuncion(document, "Tipo defunción", this.getTipo_defuncion());
         integrarTablaDefuncion(document, "Dirección de defunción", this.getDireccion_defuncion());
-        integrarTablaDefuncion(document, "Area de defunción", this.getArea_defuncion());
+        integrarTablaDefuncion(document, "Mujeres en fertelidad antes de defunción", this.getMujeres());
         integrarTablaDefuncion(document, "Area de defunción", this.getArea_defuncion());
         try {
             System.out.println("*****************");
