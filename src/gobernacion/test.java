73f890f41ed7e305786d63c513b4913503eb03c9
darkdrei <darkdrei88@gmail.com>
2017-10-30 00:24:23 -0500

    actualizacion

--- src/gobernacion/test.java
+++ src/gobernacion/test.java
@@ -6,7 +6,18 @@
 package gobernacion;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import logica.FileCopy;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
  *
@@ -18,12 +29,36 @@ public class test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        /*File f = new File("/home/dark/proyectos/AnalizadorEstadisticasVitales/data/San Jacinto/nacimientos.xls");
-        System.out.println(f.isFile());*/
-        System.out.println("CUÁDRUPLE O MÁS");
-        FileCopy c = new FileCopy();
-        c.fileCopy("/home/dark/proyectos/AnalizadorEstadisticasVitales/data/calidad.xlsx","/home/dark/proyectos/AnalizadorEstadisticasVitales/data/calidad2.xlsx");
+        InputStream excelFile = null;
+        try {
+            // TODO code application logic here
+            /*File f = new File("/home/dark/proyectos/AnalizadorEstadisticasVitales/data/San Jacinto/nacimientos.xls");
+            System.out.println(f.isFile());*/
+            String ruta = "/home/dark/proyectos/AnalizadorEstadisticasVitales/data/calidad.xlsx";
+            System.out.println("CUÁDRUPLE O MÁS");
+            FileCopy c = new FileCopy();
+            c.fileCopy("/home/dark/proyectos/AnalizadorEstadisticasVitales/data/calidad.xlsx", "/home/dark/proyectos/AnalizadorEstadisticasVitales/data/calidad2.xlsx");
+            excelFile = new FileInputStream(ruta);
+            XSSFWorkbook wb = new XSSFWorkbook(excelFile);
+            XSSFSheet sheet = wb.getSheetAt(0);
+            XSSFRow row;
+            XSSFCell cell;
+            sheet.getRow(1).getCell(2).setCellValue("VIRGILIO");
+            FileOutputStream out = new FileOutputStream(ruta);
+            wb.write(out);
+            out.flush();
+            out.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(test.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(test.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                excelFile.close();
+            } catch (IOException ex) {
+                Logger.getLogger(test.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
-    
+
 }
