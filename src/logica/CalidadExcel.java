c470d59af562f183d43d2b77deee50857af444b8
darkdrei <darkdrei88@gmail.com>
2017-11-05 17:11:21 -0500

    metodo de get celda, se encarga de extraer el calculo de los datos

--- src/logica/CalidadExcel.java
+++ src/logica/CalidadExcel.java
@@ -41,69 +41,69 @@ public class CalidadExcel extends Reporte {
             String path = "";
             String basePath = new File("").getAbsolutePath();
             if (System.getProperty("os.name").contains("Linux")) {
-                path = this.getPath() + "/" +"calidad_"+m.getMunicipio(this.getMunicipio()) + ".xlsx";
-                c.fileCopy(basePath+ "/data/calidad.xlsx", path);
+                path = this.getPath() + "/" + "calidad_" + m.getMunicipio(this.getMunicipio()) + ".xlsx";
+                c.fileCopy(basePath + "/data/calidad.xlsx", path);
             } else {
-                path = this.getPath() + "\\" +"calidad_"+ m.getMunicipio(this.getMunicipio()) + ".xlsx";
+                path = this.getPath() + "\\" + "calidad_" + m.getMunicipio(this.getMunicipio()) + ".xlsx";
                 c.fileCopy(basePath + "\\data\\calidad.xlsx", path);
             }
             File file = new File(path);
             //OPRTUNIDAD
             double oportunidad_nacimiento = this.getRegistros_oportunos_nacimientos() / this.getTotal_nacimientos();
-            double oportunidad_defunciones = this.getRegistros_oportunos_defunciones()/ this.getTotal_defunciones();
+            double oportunidad_defunciones = this.getRegistros_oportunos_defunciones() / this.getTotal_defunciones();
             float tem_mayor_nac = 0, tem_may_def = 0;
             if (this.getArea_nacimiento().size() > tem_mayor_nac) {
                 tem_mayor_nac = this.getArea_nacimiento().size();
             }
-             if (this.getSitio_nacimiento().size() > tem_mayor_nac) {
+            if (this.getSitio_nacimiento().size() > tem_mayor_nac) {
                 tem_mayor_nac = this.getSitio_nacimiento().size();
             }
-             if (this.getSemana_gestacion().size() > tem_mayor_nac) {
+            if (this.getSemana_gestacion().size() > tem_mayor_nac) {
                 tem_mayor_nac = this.getSemana_gestacion().size();
             }
-             if (this.getPeso().size() > tem_mayor_nac) {
+            if (this.getPeso().size() > tem_mayor_nac) {
                 tem_mayor_nac = this.getPeso().size();
             }
-             if (this.getPeso_tiempo_gestacion().size() > tem_mayor_nac) {
+            if (this.getPeso_tiempo_gestacion().size() > tem_mayor_nac) {
                 tem_mayor_nac = this.getPeso_tiempo_gestacion().size();
             }
-             if (this.getPeso_tiempo_gestacion_talla().size() > tem_mayor_nac) {
+            if (this.getPeso_tiempo_gestacion_talla().size() > tem_mayor_nac) {
                 tem_mayor_nac = this.getPeso_tiempo_gestacion_talla().size();
             }
-             if (this.getGrupo_sanguineo().size() > tem_mayor_nac) {
+            if (this.getGrupo_sanguineo().size() > tem_mayor_nac) {
                 tem_mayor_nac = this.getGrupo_sanguineo().size();
             }
-             if (this.getFactor_rh().size() > tem_mayor_nac) {
+            if (this.getFactor_rh().size() > tem_mayor_nac) {
                 tem_mayor_nac = this.getFactor_rh().size();
             }
-             if (this.getDireccion().size() > tem_mayor_nac) {
+            if (this.getDireccion().size() > tem_mayor_nac) {
                 tem_mayor_nac = this.getDireccion().size();
             }
-             if (this.getEdad_padre().size() > tem_mayor_nac) {
+            if (this.getEdad_padre().size() > tem_mayor_nac) {
                 tem_mayor_nac = this.getEdad_padre().size();
             }
-             if (this.getEstado().size() > tem_mayor_nac) {
+            if (this.getEstado().size() > tem_mayor_nac) {
                 tem_mayor_nac = this.getEstado().size();
             }
-             if (this.getMultiplicidad().size() > tem_mayor_nac) {
+            if (this.getMultiplicidad().size() > tem_mayor_nac) {
                 tem_mayor_nac = this.getMultiplicidad().size();
             }
             if (tem_may_def < this.getArea_defuncion().size()) {
                 tem_may_def = this.getArea_defuncion().size();
             }
-             if (tem_may_def < this.getTipo_defuncion().size()) {
+            if (tem_may_def < this.getTipo_defuncion().size()) {
                 tem_may_def = this.getTipo_defuncion().size();
             }
-             if (tem_may_def < this.getDireccion_defuncion().size()) {
+            if (tem_may_def < this.getDireccion_defuncion().size()) {
                 tem_may_def = this.getDireccion_defuncion().size();
             }
-             if (tem_may_def < this.getMujeres().size()) {
+            if (tem_may_def < this.getMujeres().size()) {
                 tem_may_def = this.getMujeres().size();
             }
-             if (tem_may_def < this.getTipo_profesional().size()) {
+            if (tem_may_def < this.getTipo_profesional().size()) {
                 tem_may_def = this.getTipo_profesional().size();
             }
-             if (tem_may_def < this.getEstado_defuncion().size()) {
+            if (tem_may_def < this.getEstado_defuncion().size()) {
                 tem_may_def = this.getEstado_defuncion().size();
             }
             InputStream excelFile = null;
@@ -133,12 +133,12 @@ public class CalidadExcel extends Reporte {
             ManejadorMunicipio muni = new ManejadorMunicipio();
             sheet.getRow(pos).getCell(3).setCellValue(hosp);
             sheet.getRow(pos).getCell(2).setCellValue(muni.getMunicipio(this.getMunicipio()));
-            float res_oport_naci = this.getRegistros_oportunos_nacimientos()  / this.getTotal_nacimientos();
-            float res_oport_defu = this.getRegistros_oportunos_defunciones()  / this.getTotal_defunciones();
-            float calidad_naci = (this.getTotal_nacimientos() - tem_mayor_nac)  / this.getTotal_nacimientos();
-            System.out.println(" "+this.getTotal_nacimientos()+"   "+tem_mayor_nac);
-            float calidad_def = (this.getTotal_defunciones() - tem_may_def)  / this.getTotal_defunciones();
-            System.out.println(" calidad ---> "+tem_mayor_nac+"   "+tem_may_def);
+            float res_oport_naci = this.getRegistros_oportunos_nacimientos() / this.getTotal_nacimientos();
+            float res_oport_defu = this.getRegistros_oportunos_defunciones() / this.getTotal_defunciones();
+            float calidad_naci = (this.getTotal_nacimientos() - tem_mayor_nac) / this.getTotal_nacimientos();
+            System.out.println(" " + this.getTotal_nacimientos() + "   " + tem_mayor_nac);
+            float calidad_def = (this.getTotal_defunciones() - tem_may_def) / this.getTotal_defunciones();
+            System.out.println(" calidad ---> " + tem_mayor_nac + "   " + tem_may_def);
             sheet.getRow(1).getCell(2).setCellValue("VIRGILIO");
             sheet.getRow(pos).getCell(7).setCellValue(res_oport_naci);
             sheet.getRow(pos).getCell(8).setCellValue(res_oport_defu);
@@ -177,5 +177,86 @@ public class CalidadExcel extends Reporte {
         } catch (IOException ex) {
             Logger.getLogger(CalidadExcel.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    public CeldaCalidadExcel getCelda() {
+
+        FileCopy c = new FileCopy();
+        ManejadorMunicipio m = new ManejadorMunicipio();
+        //OPRTUNIDAD
+        double oportunidad_nacimiento = this.getRegistros_oportunos_nacimientos() / this.getTotal_nacimientos();
+        double oportunidad_defunciones = this.getRegistros_oportunos_defunciones() / this.getTotal_defunciones();
+        float tem_mayor_nac = 0, tem_may_def = 0;
+        if (this.getArea_nacimiento().size() > tem_mayor_nac) {
+            tem_mayor_nac = this.getArea_nacimiento().size();
+        }
+        if (this.getSitio_nacimiento().size() > tem_mayor_nac) {
+            tem_mayor_nac = this.getSitio_nacimiento().size();
+        }
+        if (this.getSemana_gestacion().size() > tem_mayor_nac) {
+            tem_mayor_nac = this.getSemana_gestacion().size();
+        }
+        if (this.getPeso().size() > tem_mayor_nac) {
+            tem_mayor_nac = this.getPeso().size();
+        }
+        if (this.getPeso_tiempo_gestacion().size() > tem_mayor_nac) {
+            tem_mayor_nac = this.getPeso_tiempo_gestacion().size();
+        }
+        if (this.getPeso_tiempo_gestacion_talla().size() > tem_mayor_nac) {
+            tem_mayor_nac = this.getPeso_tiempo_gestacion_talla().size();
+        }
+        if (this.getGrupo_sanguineo().size() > tem_mayor_nac) {
+            tem_mayor_nac = this.getGrupo_sanguineo().size();
+        }
+        if (this.getFactor_rh().size() > tem_mayor_nac) {
+            tem_mayor_nac = this.getFactor_rh().size();
+        }
+        if (this.getDireccion().size() > tem_mayor_nac) {
+            tem_mayor_nac = this.getDireccion().size();
+        }
+        if (this.getEdad_padre().size() > tem_mayor_nac) {
+            tem_mayor_nac = this.getEdad_padre().size();
+        }
+        if (this.getEstado().size() > tem_mayor_nac) {
+            tem_mayor_nac = this.getEstado().size();
+        }
+        if (this.getMultiplicidad().size() > tem_mayor_nac) {
+            tem_mayor_nac = this.getMultiplicidad().size();
+        }
+        if (tem_may_def < this.getArea_defuncion().size()) {
+            tem_may_def = this.getArea_defuncion().size();
+        }
+        if (tem_may_def < this.getTipo_defuncion().size()) {
+            tem_may_def = this.getTipo_defuncion().size();
+        }
+        if (tem_may_def < this.getDireccion_defuncion().size()) {
+            tem_may_def = this.getDireccion_defuncion().size();
+        }
+        if (tem_may_def < this.getMujeres().size()) {
+            tem_may_def = this.getMujeres().size();
+        }
+        if (tem_may_def < this.getTipo_profesional().size()) {
+            tem_may_def = this.getTipo_profesional().size();
+        }
+        if (tem_may_def < this.getEstado_defuncion().size()) {
+            tem_may_def = this.getEstado_defuncion().size();
+        }
+        String hosp = "";
+        cli:
+        for (String s : this.getInstituciones_de_salud_nacimiento()) {
+            if (s.contains("HOSPITAL/CL�NICA") || s.contains("HOSPITAL")) {
+                hosp = s;
+                break cli;
+            }
+        }
+        
+        float res_oport_naci = this.getRegistros_oportunos_nacimientos() / this.getTotal_nacimientos();
+        float res_oport_defu = this.getRegistros_oportunos_defunciones() / this.getTotal_defunciones();
+        float calidad_naci = (this.getTotal_nacimientos() - tem_mayor_nac) / this.getTotal_nacimientos();
+        float calidad_def = (this.getTotal_defunciones() - tem_may_def) / this.getTotal_defunciones();
+        return new CeldaCalidadExcel(m.getMunicipio(this.getMunicipio()),
+                hosp, 
+                res_oport_naci, 
+                res_oport_defu, calidad_naci, calidad_def);
     }
 }
