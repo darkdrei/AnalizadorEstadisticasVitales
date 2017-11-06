3c046aa174db3482e4cbf9d1f631217871074b8a
darkdrei <darkdrei88@gmail.com>
2017-11-05 19:03:52 -0500

    se agregan los atributos de oportunidad y se modifica en el contructor, de calidad a oportunidad

--- src/logica/CeldaCalidadExcel.java
+++ src/logica/CeldaCalidadExcel.java
@@ -43,10 +43,108 @@ public class CeldaCalidadExcel {
         this.periodo = "";
         this.cobertura_vivo = oportunidad_vivo;
         this.cobertura_defuncion = oportunidad_defuncion;
+        this.oportunidad_vivo = oportunidad_vivo;
+        this.oportunidad_defuncion = oportunidad_defuncion;
         this.calidad_vivo = calidad_vivo;
         this.calidad_defunciones = calidad_defunciones;
         this.responsable = "";
         this.observeciones = "";
+    }
+
+    public String getZode() {
+        return zode;
+    }
+
+    public void setZode(String zode) {
+        this.zode = zode;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public String getEntidad() {
+        return entidad;
+    }
+
+    public void setEntidad(String entidad) {
+        this.entidad = entidad;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
+
+    public float getCobertura_vivo() {
+        return cobertura_vivo;
+    }
+
+    public void setCobertura_vivo(float cobertura_vivo) {
+        this.cobertura_vivo = cobertura_vivo;
+    }
+
+    public float getCobertura_defuncion() {
+        return cobertura_defuncion;
+    }
+
+    public void setCobertura_defuncion(float cobertura_defuncion) {
+        this.cobertura_defuncion = cobertura_defuncion;
+    }
+
+    public float getOportunidad_vivo() {
+        return oportunidad_vivo;
+    }
+
+    public void setOportunidad_vivo(float oportunidad_vivo) {
+        this.oportunidad_vivo = oportunidad_vivo;
+    }
+
+    public float getOportunidad_defuncion() {
+        return oportunidad_defuncion;
+    }
+
+    public void setOportunidad_defuncion(float oportunidad_defuncion) {
+        this.oportunidad_defuncion = oportunidad_defuncion;
+    }
+
+    public float getCalidad_vivo() {
+        return calidad_vivo;
+    }
+
+    public void setCalidad_vivo(float calidad_vivo) {
+        this.calidad_vivo = calidad_vivo;
+    }
+
+    public float getCalidad_defunciones() {
+        return calidad_defunciones;
+    }
+
+    public void setCalidad_defunciones(float calidad_defunciones) {
+        this.calidad_defunciones = calidad_defunciones;
+    }
+
+    public String getResponsable() {
+        return responsable;
+    }
+
+    public void setResponsable(String responsable) {
+        this.responsable = responsable;
+    }
+
+    public String getObserveciones() {
+        return observeciones;
+    }
+
+    public void setObserveciones(String observeciones) {
+        this.observeciones = observeciones;
     }
 
     
