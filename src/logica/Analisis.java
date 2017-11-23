06a66d178ca38b3abb6606b33784f9cbaac17e4d
darkdrei <darkdrei88@gmail.com>
2017-11-23 04:34:40 -0500

    Se remueve la clase TemporalInfo de la clase Analisis, con el fin de dar visivilidad a el recurso y pueda establecer un relacion de dependencia funcional

--- src/logica/Analisis.java
+++ src/logica/Analisis.java
@@ -265,62 +265,7 @@ public class Analisis implements OperacionesLibro, OperacionesCalidad {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public class TemporalInfo {
-
-        private String municipio = "";
-        private ArrayList<Object> vivos = new ArrayList<>();
-        private ArrayList<Object> difuntos = new ArrayList<>();
-        private ArrayList<Integer> anios_vivos = new ArrayList<>();
-        private ArrayList<Integer> anios_difuntos = new ArrayList<>();
-
-        public ArrayList<Integer> getAnios_vivos() {
-            return anios_vivos;
-        }
-
-        public void setAnios_vivos(ArrayList<Integer> anios_vivos) {
-            this.anios_vivos = anios_vivos;
-        }
-
-        public ArrayList<Integer> getAnios_difuntos() {
-            return anios_difuntos;
-        }
-
-        public void setAnios_difuntos(ArrayList<Integer> anios_difuntos) {
-            this.anios_difuntos = anios_difuntos;
-        }
-
-        public TemporalInfo() {
-        }
-
-        public TemporalInfo(String municipio) {
-            this.municipio = municipio;
-        }
-
-        public String getMunicipio() {
-            return municipio;
-        }
-
-        public void setMunicipio(String municipio) {
-            this.municipio = municipio;
-        }
-
-        public ArrayList<Object> getVivos() {
-            return vivos;
-        }
-
-        public void setVivos(ArrayList<Object> vivos) {
-            this.vivos = vivos;
-        }
-
-        public ArrayList<Object> getDifuntos() {
-            return difuntos;
-        }
-
-        public void setDifuntos(ArrayList<Object> difuntos) {
-            this.difuntos = difuntos;
-        }
-
-    }
+    
 
     public void generarWord(String ruta) {
         for (TemporalInfo t : this.getReporte()) {
