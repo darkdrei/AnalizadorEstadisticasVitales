f930eaee88a4a9f0063e11581fe004b4bdd2c3b7
darkdrei <darkdrei88@gmail.com>
2017-11-24 05:56:27 -0500

    creacion de la clase que contenga los datos del municipio

--- src/logica/RepExtMunicipioWord.java
+++ src/logica/RepExtMunicipioWord.java
@@ -5,6 +5,8 @@
  */
 package logica;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author dark
@@ -22,8 +24,30 @@ public class RepExtMunicipioWord extends ReporteExistencia{
      @Override
     public void writeFile() {
         for(TemporalInfo t: this.getData().getData()){
-        
+            System.out.println(" ---------------------------------------");
+            System.out.println(t.getMunicipio());
+            
+            System.out.println(t.getAnios_vivos());
+            System.out.println(t.getAnios_difuntos());
+            
         }
     }
     
+    public class TemporalMes{
+        private int mes;
+        private ArrayList<TemporalInfo> datos;
+        
+        public TemporalMes(int m){
+            this.mes= m;
+            this.datos = new ArrayList<>();
+        }
+       
+        public void addDato(TemporalInfo d){
+            this.datos.add(d);
+        }
+        
+        public int size(){
+            return this.datos.size();
+        }
+    }
 }
